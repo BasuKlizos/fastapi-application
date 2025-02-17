43e86c3305477eb2ddb8eef0c1899b24eb3373bd
BasuKlizos <basudev@klizos.com>
2025-02-17 12:55:21 +0530

    completed setup of development branch

---
+++ app/config.py
@@ -0,0 +1,13 @@
+import os 
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    MONGO_URI: str = os.getenv("MONGO_URI")
+    ALGORITHM: str = os.getenv("ALGORITHM")
+
+
+settings = Settings() 
--- app/database.py
+++ app/database.py
@@ -1,12 +1,9 @@
-import os
-
 from motor.motor_asyncio import AsyncIOMotorClient
-from dotenv import load_dotenv
 
-load_dotenv()
+from config import settings
 
-
-MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URI = settings.MONGO_URI
 client = AsyncIOMotorClient(MONGO_URI)
 database = client["fastapi_db"]
 users_collection = database["users"]
+trash_collections = database["trashs"]
